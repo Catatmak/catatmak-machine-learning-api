d086951a8c4619c3d63f20823c6d29e71609ee5a
rafiudd <rayhanrafiudd@gmail.com>
2023-12-13 22:05:01 +0700

    fix: change pickle to joblib

--- main.py
+++ main.py
@@ -1,21 +1,27 @@
 # api.py
 from flask import Flask, request, jsonify
 import pickle
+import joblib
+import numpy as np 
 
 app = Flask(__name__)
 
 # Load the pre-trained machine learning model
-with open('ModelFinal.pkl', 'rb') as model_file:
-    model = pickle.load(model_file)
+# with open('ModelFinal.pkl', 'rb') as model_file:
+#     model = pickle.load(model_file)
+
+model = joblib.load('ModelFinal.pkl')
 
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
         # Get input data from the request
-        data = request.get_json()
+        # data = request.get_json()
+        # input_array = np.array(["Bakso", "baksi"]).reshape(-1, 1)
+
 
         # Make predictions using the loaded model
-        prediction = model.predict(["Makan bakso"])  # Adjust this line based on your model input
+        prediction = model.predict([["Bakso", "baksi"]])  # Adjust this line based on your model input
 
         # Return the prediction as JSON
         return jsonify({'prediction': prediction.tolist()})
